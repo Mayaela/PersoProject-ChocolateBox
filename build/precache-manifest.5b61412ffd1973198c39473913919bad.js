bf8a7879841beac8353734f54cb3d3dcaa922e69
Maya Safadi <safadimaya@gmail.com>
2018-11-22 00:15:15 +0100

    version build git page stable 1.0.0

---
+++ build/precache-manifest.5b61412ffd1973198c39473913919bad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57cc4e3f694ac314fa3e",
+    "url": "/Project-ChocolateBox/static/js/runtime~main.57cc4e3f.js"
+  },
+  {
+    "revision": "4b6b5ca8d927f2a2d3cc",
+    "url": "/Project-ChocolateBox/static/js/main.4b6b5ca8.chunk.js"
+  },
+  {
+    "revision": "8f6c4875b72b46879697",
+    "url": "/Project-ChocolateBox/static/js/1.8f6c4875.chunk.js"
+  },
+  {
+    "revision": "4b6b5ca8d927f2a2d3cc",
+    "url": "/Project-ChocolateBox/static/css/main.77ae229e.chunk.css"
+  },
+  {
+    "revision": "75c1be416ed00e26bc77278ce3b2a707",
+    "url": "/Project-ChocolateBox/index.html"
+  }
+];
